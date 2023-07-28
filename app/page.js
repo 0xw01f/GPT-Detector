07db8d3a60ea5216ba595240b6b1aff2de87b2de
Jérémie _ <76235339+0xw01f@users.noreply.github.com>
2023-07-28 23:45:52 +0200

    added vercel analytics

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 
 const TextDetector = () => {
-  <Analytics />
+  
   const versionNumber = "1.0.4";
   const [inputText, setInputText] = useState('');
   const [detectedSentences, setDetectedSentences] = useState([]);
@@ -168,5 +168,5 @@ const TextDetector = () => {
   );
 };
 
-
+<Analytics />
 export default TextDetector;
